f52caf7f5ed3eb88d3dee69413f18733c99415c9
danishsalim <danishsalim321@gmail.com>
2023-04-05 22:13:51 +0530

    removal link

--- src/components/Confirm.jsx
+++ src/components/Confirm.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Confirm = () => {
   const { orderData, charge, games , setGames } = useContext(CartContext);
